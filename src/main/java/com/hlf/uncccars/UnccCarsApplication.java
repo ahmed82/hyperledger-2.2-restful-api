efebe3ab55eab6ccb68620135d4964f101d8fb46
Ahmed Al Salih <a.alsalih2@gmail.com>
2022-01-03 00:18:59 -0500

    adding the connect method

--- src/main/java/com/hlf/uncccars/UnccCarsApplication.java
+++ src/main/java/com/hlf/uncccars/UnccCarsApplication.java
@@ -5,9 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UnccCarsApplication {
+	
+	static {
+		System.setProperty("org.hyperledger.fabric.sdk.service_discovery.as_localhost", "true");
+	} 
 
 	public static void main(String[] args) {
 		SpringApplication.run(UnccCarsApplication.class, args);
+		
 	}
 
 }
--- src/main/java/com/hlf/uncccars/controller/WebController.java
+++ src/main/java/com/hlf/uncccars/controller/WebController.java
@@ -1,5 +1,51 @@
 package com.hlf.uncccars.controller;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+import org.hyperledger.fabric.gateway.Gateway;
+import org.hyperledger.fabric.gateway.Wallet;
+import org.hyperledger.fabric.gateway.Wallets;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hlf.uncccars.dto.Car;
+
+@RestController
+@RequestMapping("/api")
 public class WebController {
+	
+	// helper function for getting connected to the gateway
+	public static Gateway connect() throws Exception{
+		// Load a file system based wallet for managing identities.
+		Path walletPath = Paths.get("wallet");
+		Wallet wallet = Wallets.newFileSystemWallet(walletPath);
+		// load a CCP
+		Path networkConfigPath = Paths.get("..", "..", "test-network", "organizations", "peerOrganizations", "org1.example.com", "connection-org1.yaml");
+
+		Gateway.Builder builder = Gateway.createBuilder();
+		builder.identity(wallet, "appUser").networkConfig(networkConfigPath).discovery(true);
+		return builder.connect();
+	} 
+	
+	
+	@GetMapping("/cars")
+	public List<Car> getAllCars(){
+		return null;
+	}
+	
+	@GetMapping("/car")
+	public Car getCar(){
+		return null;
+	}
+	
+	@PostMapping("/car")
+	public ResponseEntity<String> CreateCar(){
+		return ResponseEntity.ok("Hello World!");
+	}
 
 }
