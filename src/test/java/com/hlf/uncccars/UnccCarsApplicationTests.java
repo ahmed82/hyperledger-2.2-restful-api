b568a0a88f53cb424ce262a0a17702b6fb24b7e6
Ahmed Al Salih <a.alsalih2@gmial.com>
2022-06-16 10:25:16 -0400

    remove the test import for security patching
    Signed-off-by: Ahmed Al Salih <a.alsalih2@gmial.com>

--- src/test/java/com/hlf/uncccars/UnccCarsApplicationTests.java
+++ src/test/java/com/hlf/uncccars/UnccCarsApplicationTests.java
@@ -1,12 +1,7 @@
 package com.hlf.uncccars;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-//@SpringBootTest
 class UnccCarsApplicationTests {
 
-	//@Test
 	void contextLoads() {
 	}
 
